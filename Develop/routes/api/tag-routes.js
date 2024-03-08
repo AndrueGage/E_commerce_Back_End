2741516e92b014c1fb29c373148beb39274490ae
AndrueGage <trainer.andrue@gmail.com>
2024-03-07 21:25:08 -0600

    updated readme and fixed tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,86 +1,102 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  router.get('/', (req, res) => {
-    Tag.findAll({ include: { model: Product } })
-      .then((tags) => res.json(tags))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      });
-  });
+  Tag.findAll({
+    include:
+    {
+      model: Product
+    }
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  router.get('/:id', (req, res) => {
-    Tag.findByPk(req.params.id, { include: { model: Product } })
-      .then((tag) => {
-        if (!tag) {
-          res.status(404).json({ message: 'No tag found with this id' });
-          return;
-        }
-        res.json(tag);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      });
-  });
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-  router.post('/', (req, res) => {
-    Tag.create(req.body)
-      .then((newTag) => res.json(newTag))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      });
-  });
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(() => { res.json({ message: 'New tag created' }) })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  router.put('/:id', (req, res) => {
-    Tag.update(req.body, { where: { id: req.params.id } })
-      .then((updatedTag) => {
-        if (!updatedTag[0]) {
-          res.status(404).json({ message: 'No tag found with this id' });
-          return;
-        }
-        res.json(updatedTag);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      });
-  });
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'Cannot update - No tag found with this id' });
+        return;
+      }
+      res.json({ message: 'Tag updated' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  router.delete('/:id', (req, res) => {
-    Tag.destroy({ where: { id: req.params.id } })
-      .then((deletedTag) => {
-        if (!deletedTag) {
-          res.status(404).json({ message: 'No tag found with this id' });
-          return;
-        }
-        res.json(deletedTag);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      });
-  });
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'Cannot delete - No tag found with this id' });
+        return;
+      }
+      res.json({ message: 'Tag deleted' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# E-commerce Back End Starter Code
+  # E Commerce Backend 
+
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+  ## Description
+  An E-Commerce back end created using Express.js, MySQL2, Sequelize, and dotenv. 
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+ Clone my repo and npm install the node packages. Use npm run seed to seed the database. To start the server use npm start
+
+  ## Usage
+ To check the contents in your database and ensure that your crud operations are working properly.
+
+ ## License
+ [MIT](https://opensource.org/licenses/MIT)
+
+  ## Questions
+  If you have any questions, contact me on [Github](https://github.com/AndrueGage/) or email me at trainer.andrue@gmail.com
